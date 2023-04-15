21c3b03a1f246a90db93c565c2b9e03546d6462f
Martina Magued <96735308+2Martina@users.noreply.github.com>
2023-04-15 10:06:27 +0200

    Add files via upload

---
+++ source/Components/Add_Sub.vhd
@@ -0,0 +1,25 @@
+Library IEEE;
+use ieee.std_logic_1164.all;
+USE IEEE.numeric_std.all;
+
+ENTITY Add_Sub IS
+PORT (
+op2,sel: IN std_logic;
+op1: in std_logic_vector (15 downto 0);
+output: out std_logic_vector (15 downto 0)  
+);
+end ENTITY Add_Sub;
+
+ARCHITECTURE Imp OF Add_Sub IS
+signal Op2_extend:  std_logic_vector (15 downto 0);
+BEGIN
+op2_extend<=  (0=>op2, Others => '0');
+
+--if push=1 --> sel=1 & subtract 1
+--if pop=1 --> sel=0 & add 1
+--if push=pop=0 --> sel=1 & subtract 0 
+
+output <= std_logic_vector(unsigned(op1) + unsigned(op2_extend)) when sel<='0'
+else  std_logic_vector(unsigned(op1) - unsigned(op2_extend));
+
+END Imp;
