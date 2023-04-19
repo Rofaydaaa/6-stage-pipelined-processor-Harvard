aa5783cd694d9d7a7d4f2ba7ecf1d7ed256edd9c
Rofaydaaa <125312170+Rofaydaaa@users.noreply.github.com>
2023-04-19 05:28:00 +0200

    Update Mem_WB_Buffer.vhd

--- source/Mem_WB_Buffer.vhd
+++ source/Mem_WB_Buffer.vhd
@@ -22,8 +22,8 @@ portFlagout:out std_logic;
 returnOIout:out std_logic;
 callout:out std_logic;
 
-IN_Port_IN: in std_logic_vector (15 downto 0);
-IN_Port_OUT: out std_logic_vector (15 downto 0)
+--IN_Port_IN: in std_logic_vector (15 downto 0);
+--IN_Port_OUT: out std_logic_vector (15 downto 0)
 );
 END Mem_WB_Buffer;
 
@@ -43,7 +43,7 @@ Intout<='0';
 portFlagout<='0';
 returnOIout<='0';
 callout<='0';
-IN_Port_OUT <= (OTHERS=>'0');
+--IN_Port_OUT <= (OTHERS=>'0');
 
 ELSIF falling_edge(Clk) THEN
 if (en='1') then
@@ -56,7 +56,7 @@ Intout<=Int;
 portFlagout<=portFlag;
 returnOIout<=returnOI;
 callout<=call;
-IN_Port_OUT <= IN_Port_IN;
+--IN_Port_OUT <= IN_Port_IN;
 
 end if;
 END IF;
