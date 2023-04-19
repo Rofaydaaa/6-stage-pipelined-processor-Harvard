2034a2f0567071188912209262a29aa4f20b45cb
Rofaydaaa <125312170+Rofaydaaa@users.noreply.github.com>
2023-04-19 05:27:17 +0200

    Update Fetch_Decode_Buffer.vhd

--- source/Fetch_Decode_Buffer.vhd
+++ source/Fetch_Decode_Buffer.vhd
@@ -6,8 +6,8 @@ PORT( Clk,Rst,en: IN std_logic;
 Instruction_IN: in std_logic_vector (31 downto 0);
 Instruction_After: out std_logic_vector (31 downto 0);
 
-IN_Port_IN: in std_logic_vector (15 downto 0);
-IN_Port_OUT: out std_logic_vector (15 downto 0)
+--IN_Port_IN: in std_logic_vector (15 downto 0);
+--IN_Port_OUT: out std_logic_vector (15 downto 0)
 );
 END Fetch_Decode_Buffer;
 
@@ -18,11 +18,11 @@ PROCESS (Clk,Rst)
 BEGIN
 IF Rst = '1' THEN
 Instruction_After <= (OTHERS=>'0');
-IN_Port_OUT <= (OTHERS=>'0');
+--IN_Port_OUT <= (OTHERS=>'0');
 ELSIF falling_edge(Clk) THEN
 if (en='1') then
 Instruction_After <= Instruction_IN;
-IN_Port_OUT <= IN_Port_IN;
+--IN_Port_OUT <= IN_Port_IN;
 end if;
 END IF;
 END PROCESS;
