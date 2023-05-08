c40c1898a330b8468fe5986aff9968f222ff435c
yasmiinezaki <71704853+yasmiinezaki@users.noreply.github.com>
2023-05-09 00:28:31 +0300

    update add to pc

--- source/Stages/Fetch.vhd
+++ source/Stages/Fetch.vhd
@@ -12,15 +12,23 @@ M_Of_1: IN std_logic_vector (15 downto 0);
 WB_Rdst: IN std_logic_vector (15 downto 0);
 WB_data: IN std_logic_vector (15 downto 0);
 For_Call: OUT std_logic_vector (15 downto 0);
+Imm_From_Decode_stage: IN std_logic;
 Instruction: out std_logic_vector (31 downto 0)
 );
 END ENTITY Fetch;
 
 ARCHITECTURE Imp OF Fetch IS
 
-Signal pc_out,Add_sub_Result,To_PC: std_logic_vector (15 downto 0);
+Signal pc_out,Add_sub_Result1,To_PC,Add_sub_Result2,Add_sub_Result_final: std_logic_vector (15 downto 0);
 Signal Int_from_Int_Reg: std_logic;
 Signal Intermediate_Inst: std_logic_vector (31 downto 0);
+
+COMPONENT Mux2by1 IS 
+Generic ( n : Integer:=16);
+PORT ( in0,in1 : IN std_logic_vector (n-1 DOWNTO 0);
+        sel : IN  std_logic;
+        out1 : OUT std_logic_vector (n-1 DOWNTO 0));
+END COMPONENT;
 
 BEGIN
 
@@ -28,14 +36,16 @@ PC0 :entity work.PC port map (freeze_hdu_data,freeze_hdu_structural,Rst,Intermed
 
 Int_Reg0 : entity work.Interrupt_register port map (Int_port,Int_from_Int_Reg);
 
-AddSUB0 : entity work.Add_Sub port map ('1','0',pc_out,Add_sub_Result);
+AddSUB0 : entity work.Add_Sub port map ('0',b"01",pc_out,Add_sub_Result1);
+AddSUB1:  entity work.Add_Sub port map ('0',b"10",pc_out,Add_sub_Result2);
+Mux2by10 : Mux2by1 GENERIC MAP(16) PORT MAP(Add_sub_Result1, Add_sub_Result2, Imm_From_Decode_stage, Add_sub_Result_final);
 
-ToPCDecisionUnit0: entity work.ToPCDecisionUnit port map (Add_sub_Result,Branch_Addr,M_Of_1,WB_Rdst,WB_data,Int_from_WB,Call,Ret,Branch,To_PC);
+ToPCDecisionUnit0: entity work.ToPCDecisionUnit port map (Add_sub_Result_final,Branch_Addr,M_Of_1,WB_Rdst,WB_data,Int_from_WB,Call,Ret,Branch,To_PC);
 
 Instruction_Mem0 : entity work.Instruction_Mem port map (clk,Rst,Int_from_Int_Reg,pc_out,Intermediate_Inst);
 
 Instruction <= Intermediate_Inst;
 
-For_Call <= Add_sub_Result;
+For_Call <= Add_sub_Result_final;
 
 END Imp;
