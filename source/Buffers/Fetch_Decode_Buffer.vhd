a52f863bb1dbcc6cb18296b910c7ff5c191b71dc
yasmiinezaki <71704853+yasmiinezaki@users.noreply.github.com>
2023-05-08 23:48:34 +0300

    Update Fetch_Decode_Buffer.vhd

--- source/Buffers/Fetch_Decode_Buffer.vhd
+++ source/Buffers/Fetch_Decode_Buffer.vhd
@@ -5,10 +5,10 @@ ENTITY Fetch_Decode_Buffer IS
 PORT( Clk,Rst,en,Int_from_Int_reg_in: IN std_logic;
 Instruction_IN: in std_logic_vector (31 downto 0);
 Instruction_After: out std_logic_vector (31 downto 0);
-Int_from_Int_reg_out: out std_logic
+Int_from_Int_reg_out: out std_logic;
+IN_Port_IN: in std_logic_vector (15 downto 0);
+IN_Port_OUT: out std_logic_vector (15 downto 0)
 
---IN_Port_IN: in std_logic_vector (15 downto 0);
---IN_Port_OUT: out std_logic_vector (15 downto 0)
 );
 END Fetch_Decode_Buffer;
 
@@ -20,12 +20,12 @@ BEGIN
 IF Rst = '1' THEN
 Instruction_After <= (OTHERS=>'0');
 Int_from_Int_reg_out <= '0';
---IN_Port_OUT <= (OTHERS=>'0');
+IN_Port_OUT <= (OTHERS=>'0');
 ELSIF falling_edge(Clk) THEN
 if (en='1') then
 Int_from_Int_reg_out <= Int_from_Int_reg_in;
 Instruction_After <= Instruction_IN;
---IN_Port_OUT <= IN_Port_IN;
+IN_Port_OUT <= IN_Port_IN;
 end if;
 END IF;
 END PROCESS;
